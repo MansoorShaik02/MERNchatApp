7c43c4114055443316f4c586575716dda7790936
mansoor <mansoorshaik7b@gmail.com>
2024-11-05 20:17:18 +0530

    build done

--- Backend/server.js
+++ Backend/server.js
@@ -1,33 +1,35 @@
-import express from "express"
-import dotenv from 'dotenv'
-import cookieParser from "cookie-parser"
-import authRoutes from './Routes/auth.routes.js'
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
-import messageRoutes from './Routes/message.routes.js'
-import userRoutes from './Routes/user.routes.js'
-import cors from 'cors';
-import { app, server } from '../Backend/socket/socket.js'
-const PORT = process.env.PORT || 5000
+import { app, server } from "./socket/socket.js";
 
+dotenv.config();
 
-const corsOptions = {
-    origin: 'http://localhost:3000', // Your frontend domain, adjust if different
-    credentials: true, // Allow credentials such as cookies and authorization headers
-};
-app.use(cors(corsOptions));
-dotenv.config()
+const __dirname = path.resolve();
+// PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
+const PORT = process.env.PORT || 5000;
 
-app.use(express.json()) // to parse the incoming requests from req.body in json format
-app.use(cookieParser())
-app.use("/api/messages", messageRoutes)
-app.use("/api/auth", authRoutes)
-app.use("/api/users", userRoutes)
-// app.get("/", (req, res) => {
-//     res.send("Hello world yahoo")
-// })
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(cookieParser());
 
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
     connectToMongoDB();
-    console.log(`server running on port33333 ${PORT}`)
+    console.log(`Server Running on port ${PORT}`);
 });
